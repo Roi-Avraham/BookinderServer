1fc65c576f678b867a9d7875ce4f6db233966338
Roi Avraham <roicvc5@gmail.com>
2022-12-17 21:27:02 +0200

    start

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     writer_name = db.Column(db.String(100), nullable=False)
-    genre = db.Column(db.String(80), nullable=False)
+    genre = db.Column(db.String(80))
 
     def __repr__(self):
         return f'<Book {self.name}>'
