c8c5bca1d986214ce482b113f49e385d44a7476f
yfishman <yfishman@salesforce.com>
2022-12-28 22:07:28 +0200

    canges

--- app.py
+++ app.py
@@ -9,23 +9,12 @@ from sqlalchemy.sql import func
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
-    "mysql://root:toor@localhost:3306/db_bookinder"
+    f"mysql://root:{os.getenv('MYSQL_PASSWORD')}@localhost:3306/db_bookinder"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ############################################################################
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(80), unique=True, nullable=False)
-    age = db.Column(db.Integer)
-    phone_number = db.Column(db.String(100), nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
-
-
+from users import *
 class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
